bdedf893af3a36e0e6e1972f15fa0ff8d3747ad5
aleksawashere <aleksa.dimitrijevic.business@gmail.com>
2022-08-27 13:14:32 +0200

    UI homepage

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -11,20 +11,32 @@ export class HomePage {
   todoList = [
     {
       itemName: 'Coding',
-      itemDueDate: '13-10-21',
+      itemDueDate: '10-13-21',
       itemPriority: 'high',
       itemCategory: 'Work'
     },
     {
       itemName: 'Painting',
-      itemDueDate: '28-10-21',
+      itemDueDate: '10-28-21',
       itemPriority: 'low',
       itemCategory: 'Personal'
     },
     {
       itemName: 'Shopping',
-      itemDueDate: '30-10-21',
+      itemDueDate: '10-30-21',
       itemPriority: 'middle',
+      itemCategory: 'Personal'
+    },
+    {
+      itemName: 'Design',
+      itemDueDate: '10-20-21',
+      itemPriority: 'high',
+      itemCategory: 'Work'
+    },
+    {
+      itemName: 'Drink with friends',
+      itemDueDate: '10-17-21',
+      itemPriority: 'low',
       itemCategory: 'Personal'
     },
 ]
